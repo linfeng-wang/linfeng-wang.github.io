66ed41b01ecf1926b3b711a1fbdbc90d9ff91c74
linfeng-wang <52597300+linfeng-wang@users.noreply.github.com>
2025-07-31 20:15:53 +0000

    Deploying to gh-pages from @ linfeng-wang/linfeng-wang.github.io@b45b83f485418ad74b59dc67643befb03f971744 🚀

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,14 +1,8 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const mode_toggle = document.getElementById("light-toggle");
-
-//     mode_toggle.addEventListener("click", function() {
-//         toggleTheme(localStorage.getItem("theme"));
-//     });
-// });
-
-// darkmode.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Disable dark mode toggle
-    localStorage.setItem("theme", "dark");
-    document.documentElement.setAttribute("data-theme", "dark");
+    const mode_toggle = document.getElementById("light-toggle");
+
+    mode_toggle.addEventListener("click", function() {
+        toggleTheme(localStorage.getItem("theme"));
+    });
 });
+
